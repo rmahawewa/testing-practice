71f10a60ff47d51b1cc020117ee02ddf4f525294
rmahawewa <rmahawewa@yahoo.com>
2025-01-22 16:52:06 +0530

    test basic functionalities of a calculator

--- one.test.js
+++ one.test.js
@@ -24,3 +24,39 @@ function reverseString(word){
 test('reversed word', () => {
     expect(reverseString('reversed word')).toMatch(/drow desrever/);
 });
+
+
+function calculator(){
+    let add = (number1, number2) => {
+        return (number1+number2);
+    }
+    let subtract = (number1, number2) => {
+        return (number1-number2);
+    }
+    let divide = (number1, number2) => {
+        return (number1/number2);
+    }
+    let multiply = (number1, number2) => {
+        return (number1*number2);
+    }
+    return {add, subtract, divide, multiply};
+}
+
+let calculator1 = calculator();
+
+test('adds 10 + 20 to equal 30', () => {
+    expect(calculator1.add(10,20)).toBe(30);
+});
+
+test('subtracts 20-10 to equal 10', () => {
+    expect(calculator1.subtract(20,10)).toBe(10);
+});
+
+test('devides 30/10 to equal 3', () => {
+    expect(calculator1.divide(30,10)).toBe(3);
+});
+
+test('multiply 10*20 to equal 200', () => {
+    expect(calculator1.multiply(10,20)).toBe(200);
+});
+
